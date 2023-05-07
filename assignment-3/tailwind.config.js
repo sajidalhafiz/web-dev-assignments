81b35f973ee6e3afd34c1a0dad8e87bc84e1edae
tryit-100 <safwan.std21@gmail.com>
2023-05-07 23:55:34 +0600

    banner content added

---
+++ assignment-3/tailwind.config.js
@@ -0,0 +1,14 @@
+
+
+module.exports = {
+    theme: {
+      fontFamily: {
+        sans: ['ui-sans-serif', 'system-ui'],
+        serif: ['ui-serif', 'Georgia'],
+        mono: ['ui-monospace', 'SFMono-Regular'],
+        display: ['Oswald'],
+        heading: "'EB Garamond', serif'",
+        paragraph: "'Poppins', sans-serif'"
+      }
+    }
+  }
